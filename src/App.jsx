acacffbc5c6b35e9030fe434cdedca7d7b78d1d0
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-12 21:19:45 +1100

    Convert navbar title to component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginControl from 'authentication/LoginControl';
+import Title from 'layout/Title';
 import PatientEditorPage from './patient/PatientEditorPage';
 import LoginPage from './authentication/LoginPage';
 import styles from './App.module.css';
@@ -9,7 +10,7 @@ function App() {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <span className={styles.appTitle}>Pharma</span>
+        <Title />
         <LoginControl />
       </header>
       <article className={styles.article}>
---
+++ src/layout/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Title.module.css';
+
+const Title = () => (
+  <h1 className={styles.title}>
+      Pharma
+  </h1>
+);
+
+export default Title;
