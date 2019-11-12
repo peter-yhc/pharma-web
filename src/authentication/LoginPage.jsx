31c664f2ea9e2ce54aeb7827eb1f25ab4e6eb469
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-12 23:06:39 +1100

    Add sign in form.

--- src/authentication/LoginPage.jsx
+++ src/authentication/LoginPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styles from './LoginPage.module.css';
 
 const LoginPage = () => (
-  <>
-    <span>Login Page</span>
-  </>
+  <section className={styles.page}>
+    <h2 className={styles.pageTitle}>
+      Sign in
+    </h2>
+    <form className={styles.form}>
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="Password" />
+      <button className={styles.signInButton} type="submit">
+        Sign in
+      </button>
+    </form>
+  </section>
 );
 
 export default LoginPage;
