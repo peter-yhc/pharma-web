d81d9300c3e2eb6a04bfaff021d4cf670be9c1eb
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-12 15:40:09 +1100

    Add initial API impl.

--- src/patient/PatientEditorPage.jsx
+++ src/patient/PatientEditorPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import withAuthentication from "authentication/withAuthentication";
+import {getPatients} from "api/PharmaApi";
 
 const PatientEditorPage = () => {
+  const [patients, setPatients] = useState([])
 
+  useEffect(() => {
+    getPatients().then((data) => {
+      setPatients(data)
+    })
+  }, [])
+
+  console.log(patients)
   return (
     <>
       <span>Patient Page</span>
