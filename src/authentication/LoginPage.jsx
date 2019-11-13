f27838e1438846223120eb97487e82cb8976196e
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-13 16:26:55 +1100

    Move signup link up.

--- src/authentication/LoginPage.jsx
+++ src/authentication/LoginPage.jsx
@@ -15,10 +15,10 @@ const LoginPage = () => {
       <h2 className={styles.pageTitle}>
         Sign in
       </h2>
+      <a className={styles.signupLink} href="/signup">Need an account?</a>
       <form className={styles.form} onSubmit={handleSubmit}>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <a className={styles.signupLink} href="/signup">Need an account?</a>
         <button className={styles.signInButton} type="submit">
           Sign in
         </button>
