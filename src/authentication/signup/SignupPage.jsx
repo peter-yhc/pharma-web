f344f0b7a14df141da0e93302b427544de35c87f
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-13 22:17:51 +1100

    Add redirect on success.

--- src/authentication/signup/SignupPage.jsx
+++ src/authentication/signup/SignupPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Input, PageTitle, Button } from 'common';
+import { Redirect } from 'react-router-dom';
 import styles from './SignupPage.module.scss';
 
 const SignupPage = () => {
@@ -7,6 +8,7 @@ const SignupPage = () => {
   const [email, setEmail] = useState(undefined);
   const [authKey, setAuthKey] = useState(undefined);
   const [disabled, setDisabled] = useState(true);
+  const [signupSuccess, setSignupSuccess] = useState(false);
   const formRef = useRef();
 
   const handleUsernameChange = (e) => {
@@ -21,6 +23,11 @@ const SignupPage = () => {
     setAuthKey(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSignupSuccess(true);
+  };
+
   useEffect(() => {
     const anyFieldsEmpty = (!username || !email || !authKey);
     const invalidForm = (formRef.current && !formRef.current.checkValidity());
@@ -28,19 +35,23 @@ const SignupPage = () => {
   }, [username, email, authKey]);
 
   return (
-    <section className={styles.page}>
-      <PageTitle>Sign up</PageTitle>
-      <form className={styles.form} ref={formRef}>
-        <p>
+    signupSuccess
+      ? <Redirect to="/login" />
+      : (
+        <section className={styles.page}>
+          <PageTitle>Sign up</PageTitle>
+          <form className={styles.form} ref={formRef} onSubmit={handleSubmit}>
+            <p>
           * To sign up as an administrator you will need to have
           the authentication key of the application.
-        </p>
-        <Input type="text" placeholder="Username" onChange={handleUsernameChange} required />
-        <Input type="email" placeholder="Email" onChange={handleEmailChange} required />
-        <Input type="password" placeholder="Authentication Key" onChange={handleAuthKeyChange} required />
-        <Button className={styles.signupButton} type="submit" disabled={disabled}>Sign up</Button>
-      </form>
-    </section>
+            </p>
+            <Input type="text" placeholder="Username" onChange={handleUsernameChange} required />
+            <Input type="email" placeholder="Email" onChange={handleEmailChange} required />
+            <Input type="password" placeholder="Authentication Key" onChange={handleAuthKeyChange} required />
+            <Button className={styles.signupButton} type="submit" disabled={disabled}>Sign up</Button>
+          </form>
+        </section>
+      )
   );
 };
 
